9970734d9a238d605e6593fc9da1bfe53b79e149
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2023-09-20 23:10:02 -0300

    main

--- src/componentes/Botao/index.js
+++
@@ -1,9 +0,0 @@
-import './Botao.css';
-/* Para passar icone ou qualquer outra coisa usamos pops.children*/
-const Botao = (props) => {
-    return (<button className='botao'>
-        {props.children} 
-    </button>)
-}
-
-export default Botao;
